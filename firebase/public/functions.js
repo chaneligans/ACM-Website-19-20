612e976d2a92caa28f8656d60a32a8a64fec8296
chaneligans <37652117+chaneligans@users.noreply.github.com>
2019-09-04 03:20:07 -0700

    updated profiles

--- firebase/public/functions.js
+++ firebase/public/functions.js
@@ -3,7 +3,7 @@ $(function() {
     // h = wiewport height
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > h) {
+        if($(window).scrollTop() > (h/10)) {
             $(".header").addClass("active");
         } else {
             //remove the background property so it comes transparent again (defined in css)
